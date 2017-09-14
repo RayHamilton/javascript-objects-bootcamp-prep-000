248dc16279964912c253babcbd9b85bc64c7b289
Ray Hamilton <hamilton.ray@gmail.com>
2017-09-14 15:22:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 playlist = {Yazoo: "Goodbye 70s"}
 
 function updatePlaylist(playlist, artistName,songTitle){
-  playlist["artistName"]=songTitle
+  playlist[artistName]=songTitle
   return playlist
 }
